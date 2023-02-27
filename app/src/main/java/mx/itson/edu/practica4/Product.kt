c7e4efca7014af2b407073b549d386c099f74b28
NallelyRAM <nallely.ramirez216724@potros.itson.edu.mx>
2023-02-26 20:36:23 -0700

    Actualización

--- app/src/main/java/mx/itson/edu/practica4/Product.kt
+++
@@ -1,6 +0,0 @@
-package mx.itson.edu.practica4
-
-data class Product(var name: String,
-                    var image: Int,
-                    var description: String,
-                    var price: Double)
